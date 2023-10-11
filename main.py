81a109cb0180740e222c856e6262ca44d0905904
gitmonstera <tatosh.good@yandex.ru>
2023-10-11 14:08:49 +0300

    start

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5322324188:AAGsEpCEQCFR9DtI6bN6fv0f3CNPDCadEMk')
+
+@bot.message_handler(commands=['start'])
+def start_comand(message):
+    
+
+    markup = types.InlineKeyboardMarkup()
+    bt1 = types.InlineKeyboardButton(text='y', callback_data='yes')
+    bt2 = types.InlineKeyboardButton(text='n', callback_data='no')
+    markup.add(bt1, bt2)
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}', reply_markup=markup)
+
+@bot.message_handler(commands=['help'])    
+def main():
+    pass
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == 'yes':
+        bot.send_message(call.message.chat.id, f'Привет, {call.message.chat.first_name}' )
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop = True)
