b28076216d375b76e49ec26a3341d7d603245f18
gitmonstera <tatosh.good@yandex.ru>
2023-10-13 17:04:48 +0300

    start func bot

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import telebot
 from telebot import types
 
+
 bot = telebot.TeleBot('5322324188:AAGsEpCEQCFR9DtI6bN6fv0f3CNPDCadEMk')
 
 @bot.message_handler(commands=['start'])
 def start_comand(message):
-    markup = types.InlineKeyboardMarkup()
-    bt1 = types.InlineKeyboardButton(text='y', callback_data='yes')
-    bt2 = types.InlineKeyboardButton(text='n', callback_data='no')
-    markup.add(bt1, bt2)
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}', reply_markup=markup)
+    markup_rep = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    bt_about_ = types.KeyboardButton("О нас")
+    bt_catalog_ = types.KeyboardButton("Каталог")
+    bt_reviews_ = types.KeyboardButton("Отзывы")
+    bt_foto_ = types.KeyboardButton("Фото")
+    bt_contacts_ = types.KeyboardButton("Контакты")
+    markup_rep.add(bt_about_, bt_catalog_, bt_reviews_, bt_foto_, bt_contacts_)
+    bot.send_message(message.chat.id,
+    f'Привет, {message.from_user.first_name}. Бот ресторана Mucho рад видеть тебя если у вас возникнут какие-либо проблемы - /help',
+    reply_markup=markup_rep)
 
 @bot.message_handler(commands=['help'])
-def main():
-    pass
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def answer(call):
-    if call.data == 'yes':
-        pass
+def help(message):
+    markup_inl = types.InlineKeyboardMarkup()
+    bt_tg_connect_ = types.InlineKeyboardButton("связь в телеграме", url='https://t.me/MUCHO_REST')
+    markup_inl.add(bt_tg_connect_)
+    bot.send_message(message.chat.id, 'Возникли проблемы обращайтесь', reply_markup=markup_inl)
 
 
 if __name__ == "__main__":
